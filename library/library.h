d7e047da14dc49b06bd366debe1fc9bf76d435cf
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-01-16 16:34:20 +0500

    added nya header gif

--- library/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef GITHUB_LIBRARY_H
-#define GITHUB_LIBRARY_H
-
-void hello(void);
-
-#endif //GITHUB_LIBRARY_H
---
+++ nya.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void nya(void) {
+  printf("Hello, Nyan!\n");
+}
